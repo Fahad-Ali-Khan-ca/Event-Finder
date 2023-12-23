62fcedeb85bdf18096eb2d55c2308491e72c601e
Fahad Ali Khan <fkhan195@myseneca.ca>
2023-12-23 00:40:22 -0500

    added datetime search and search by artist name

--- app.py
+++ app.py
@@ -7,19 +7,45 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['GET'])
-def search():
-    city = request.args.get('city')  # Get city from user input
-    api_key = 'vloAti78R5VU5xlEehviuY4dgV7MWIsj'  # Replace with your actual API key
-    url = f'https://app.ticketmaster.com/discovery/v2/events.json?apikey={api_key}&city={city}'
-    print('city: ', city)
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        events = response.json().get('_embedded', {}).get('events', [])
-        return render_template('results.html', events=events, city=city)
-    else:
-        return 'Error: Unable to fetch events', 500
+# validate the form and return the results
+def validate_form(startdate, enddate):
+    #check if the user entered a start date and end date
+    if startdate and enddate:
+        #check if the end date is after the start date
+        print(startdate)
+        if startdate > enddate:
+            return render_template('index.html', error='End date should be after start date')
+        else:
+            return True
+
+
+@app.route('/search', methods=['POST','GET'])
+def search():
+    if request.method == 'POST':
+        city = request.form['city']
+        StartDateTime	 =  request.form['startdate']
+        EndDateTime = request.form['enddate']
+        Artist = request.form['artist']
+        api_key = 'vloAti78R5VU5xlEehviuY4dgV7MWIsj'  # Replace with your actual API key
+        url = f'https://app.ticketmaster.com/discovery/v2/events.json?apikey={api_key}&city={city}'
+        if StartDateTime:
+            StartDateTime += 'T00:00:00Z'
+            url += f'&startDateTime={StartDateTime}'
+        if EndDateTime:
+            EndDateTime += 'T23:59:59Z'
+            url += f'&endDateTime={EndDateTime}'
+        validate_form(StartDateTime, EndDateTime)
+        if Artist:
+            url += f'&keyword={Artist}'
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(response.json())
+            events = response.json().get('_embedded', {}).get('events', [])
+            return render_template('results.html', events=events)
+        else:
+            return 'Error: Unable to fetch events', 500
 
 if __name__ == '__main__':
     app.run(debug=True)
